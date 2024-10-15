6fb5145da870eb9cefbfe4213b76156a9a14215b
Azwad1 <155709985+Azwad1@users.noreply.github.com>
2024-10-15 11:02:43 +0530

    Automated grading system.py

--- Automated grading system.py
+++ Automated grading system.py
@@ -27,3 +27,23 @@ Sample Input:
 78
 Sample Output:C
 """
+marks = int(input())
+
+if marks > 100:
+    print("Invalid Input")
+elif marks == 100:
+    print("S")
+elif 90 <= marks < 100:
+    print("A")
+elif 80 <= marks < 90:
+    print("B")
+elif 70 <= marks < 80:
+    print("C")
+elif 60 <= marks < 70:
+    print("D")
+elif 50 <= marks < 60:
+    print("E")
+elif marks < 50:
+    print("F")
+else:
+    print("Invalid Input") 
